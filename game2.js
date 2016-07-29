24d743d6ee72c2486e8572d37126f8ad81816b89
David Li <jdavidli01@outlook.com>
2016-07-28 23:42:29 -0400

    new questions, new js file

---
+++ game2.js
@@ -0,0 +1,44 @@
+var questions = 
+[
+	{
+		question: "True or False: Topgepi appears in the first series of Pokemon.",
+		choices:["false", "true"],
+		answer: 
+	},{
+		question: "What is the first Legendary bird Pokemon Ash sees?",
+		choices: ["Moltes", "Articuno", "Zapdos", "Lugia", "Ho-oh"],
+		answer:
+	},{
+		question: "Which Pokemon game was the first to make Pokemon sprites seem to move?",
+		choices: ["Diamond", "Crystal", "Red", "Emerald"],
+		answer:
+	},{
+		question: "In which Pokemon game can you obtain all the Kanto/Indigo starters?",
+		choices: ["Blue", "Green", "Yellow", "Red"],
+		answer:
+	},{
+		question: "How any total Pokemon are there in first generation?",
+		choices: ["237", "150", "151", "649", "469"],
+		answer:
+	},{
+		question: "What was the first Pokemon Ash captured?",
+		choices: ["Squirtle", "Pidgeotto", "Pikachu" ,"Caterpie"],
+		answer:
+	},{
+		question: "Which Pokemon is involved in a theory that expresses why Cubone wears a skull on its head?"
+		choices: ["Chansey", "Kangaskan", "Rhydon", "Marowak"],
+		answer:
+	},{
+		question: "This Pokemon was said to have been born from a another Pokemon in the video game. However, in a movie, it is said to be the first Pokemon to be created by humans. Who's that Pokemon?"
+		choices: ["Jirachu", "Rhydon", "Mewtwo", "Deoxys"],
+		answer:
+	},{
+		question: "How does an Eevee evolve into an Espeon or Umbreon?"
+		choice: ["By obtaining a certain level of happiness",
+				"By using a certain stone",
+				"By leveling up",
+				"by fighting in battles"],
+		answer:
+	}
+
+]
